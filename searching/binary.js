953fa8fd011e728516e66d9397294ec3b1f34efa
Samybaxy <upwork@samybaxy.net>
2021-06-17 20:06:49 +0100

    Binary search

--- searching/binary.js
+++ searching/binary.js
@@ -1,7 +1,17 @@
-export default function(arr, val) {
-    let start = 0;
-    let end = array.length - 1;
-    let mid = Math.floor(start + end / 2);
+// Assuming the itemlist array is already sorted.
+// If not, sort the array using merge or quicksort.
+export default function (item, itemlist) {
+  let lowerIdx = 0;
+  let upperIdx = itemlist.length - 1;
+  let midIdx = null;
 
-    while ( arr[mid] < val )
+  while (lowerIdx <= upperIdx) {
+    midIdx = Math.floor((lowerIdx + upperIdx) / 2);
+    if (item === itemlist[midIdx]) return midIdx;
+
+    if (item > itemlist[midIdx]) lowerIdx = midIdx + 1;
+    else upperIdx = midIdx - 1;
+  }
+
+  if (lowerIdx > upperIdx) return -1;
 }
